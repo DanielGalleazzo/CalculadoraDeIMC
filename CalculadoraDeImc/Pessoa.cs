8e64368cb25a2346c39311c25d047655cd038b28
Daniel Augusto Galleazzo <danielgalleazzo@gmail.com>
2025-05-29 22:51:19 -0300

    primeiro commit

---
+++ CalculadoraDeImc/Pessoa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDeImc
+{
+    public class Pessoa
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public double weight { get; set; }
+        public double height { get; set; }
+
+
+        public Pessoa()
+        {
+        }
+        public Pessoa(string name, int age, double weight, double height)
+        {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.height = height;
+        }
+        public double IMC()
+        {
+            return weight / (height * height);
+        }
+
+        public string IMCtxt()
+        {
+            double imc = IMC();
+            if (imc < 0.0018)
+                return "Abaixo do peso";
+            else if (imc < 0.0025)
+                return "Peso normal";
+            else if (imc < 0.0030)
+                return "Sobrepeso";
+            else
+                return "Obesidade";
+        }
+
+
+    }
+}
---
+++ CalculadoraDeImc/Program.cs
@@ -0,0 +1,49 @@
+﻿using CalculadoraDeImc;
+List<Pessoa> listaPessoa = new List<Pessoa>();
+Console.WriteLine("Você quer consultar o IMC de uma ou várias pessoas (uma / varias )");
+string resposta  = Console.ReadLine();
+
+if (resposta.ToLower() == "varias")
+{
+    Console.WriteLine("Quantas pessoas você quer analisar ?");
+    int n1 = int.Parse(Console.ReadLine());
+    for(int i = 0; i < n1; i++)
+    {
+        Console.WriteLine("Digite o nome da " + i + " pessoa");
+        string name = Console.ReadLine();
+        Console.WriteLine("Insira a idade da " + i + " pessoa");
+        int age = int.Parse(Console.ReadLine());
+        Console.WriteLine("Insira o peso da " + i + " pessoa");
+        double weight = double.Parse(Console.ReadLine());
+        Console.WriteLine("Insira a altura da" + i + " pessoa");
+        double height = double.Parse(Console.ReadLine());
+        Pessoa pessoa = new Pessoa(name,age,weight,height);
+        listaPessoa.Add(pessoa);
+
+    }
+}
+else
+{
+    Console.WriteLine("Digite o nome da pessoa");
+    string name = Console.ReadLine();
+    Console.WriteLine("Insira a idade da  pessoa");
+    int age = int.Parse(Console.ReadLine());
+    Console.WriteLine("Insira o peso da pessoa");
+    double weight = double.Parse(Console.ReadLine());
+    Console.WriteLine("Insira a altura da pessoa");
+    double height = double.Parse(Console.ReadLine());
+    Pessoa pessoa = new Pessoa(name, age, weight, height);
+    listaPessoa.Add(pessoa);
+}
+foreach (var pe in listaPessoa)
+{
+    double imc = pe.IMC();
+    string classificacao = pe.IMCtxt();
+
+    Console.WriteLine("Nome: " + pe.name);
+    Console.WriteLine("Idade: " + pe.age);
+    Console.WriteLine("Peso: " + pe.weight);
+    Console.WriteLine("Altura: "+ pe.height);
+    Console.WriteLine("IMC: " + pe.IMC());
+    Console.WriteLine("Classificação: " + pe.IMCtxt());
+}
